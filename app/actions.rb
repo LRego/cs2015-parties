b7547543cb5b52d9895478872d6ee3fcc8ebf03c
Fábio Henriques <fabyohenriques@gmail.com>
2015-07-21 11:59:48 +0100

    create a party feature

--- app/actions.rb
+++ app/actions.rb
@@ -16,6 +16,9 @@ end
 
 # method to save a new party, the /new route should point here
 post '/create' do
+  party = Party.new name: params[:name], address: params[:address], longitude: params[:longitude], latitude: params[:latitude], time: params[:time]
+  party.save
+  redirect "/"
 end
 
 # method to update an existing party, the /:id/edit should point here
